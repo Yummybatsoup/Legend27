b82fc33820ba2f60272ef118682293d3fc460ed3
Jeffrey <thliuab@connect.ust.hk>
2020-05-05 19:10:41 +0800

    Test Case of Task 1 and 6 ( not yet finish)

---
+++ src/test/java/comp3111/coursescraper/Task1Test.java
@@ -0,0 +1,64 @@
+package comp3111.coursescraper;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Task1Test extends ApplicationTest{
+
+	private Scene s;
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+    	FXMLLoader loader = new FXMLLoader();
+    	loader.setLocation(getClass().getResource("/ui.fxml"));
+   		VBox root = (VBox) loader.load();
+   		Scene scene =  new Scene(root);
+   		stage.setScene(scene);
+   		stage.setTitle("Course Scraper");
+   		stage.show();
+   		s = scene;
+	}
+	
+	@Test
+	public void testSearchError() {
+		
+		TextField url = (TextField) s.lookup("#textfieldURL");
+		url.setText("http://w5.ab.ust.hk/wcq/cgi-bin/123");
+		TextField term = (TextField) s.lookup("#textfieldTerm");
+		term.setText("1910");
+		TextField subject = (TextField) s.lookup("#textfieldSubject");
+		subject.setText("COMP");
+		clickOn("#buttonSearch");
+		
+		TextArea console=(TextArea) s.lookup("#textAreaConsole");
+		assertTrue(console.getText().equals("Invail URL"));
+		
+	}
+	
+	@Test
+	public void testSearchDistinct() {
+		TextField url = (TextField) s.lookup("#textfieldURL");
+		url.setText("http://w5.ab.ust.hk/wcq/cgi-bin/");
+		TextField term = (TextField) s.lookup("#textfieldTerm");
+		term.setText("1910");
+		TextField subject = (TextField) s.lookup("#textfieldSubject");
+		subject.setText("COMP");
+		clickOn("#buttonSearch");
+		
+		TextArea console=(TextArea) s.lookup("#textAreaConsole");
+		boolean result = !console.getText().isBlank() && !console.getText().equals("Invail URL");
+		assertTrue(result);
+		
+		
+	}
+}
