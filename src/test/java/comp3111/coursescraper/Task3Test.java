f6b3d6c47b0f623a11df6f33e0f2a96061da8a6f
Pochingto <rcwk3756@gmail.com>
2020-05-12 18:26:29 +0800

    task 3 test completed

--- src/test/java/comp3111/coursescraper/Task3Test.java
+++ src/test/java/comp3111/coursescraper/Task3Test.java
@@ -5,9 +5,12 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
 
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
@@ -34,7 +37,7 @@ public class Task3Test extends ApplicationTest {
 
 	
 	@Test
-	public void test() {
+	public void Task3NoEnrolltest() {
 		TextField url = (TextField) s.lookup("#textfieldURL");
 		url.setText("http://w5.ab.ust.hk/wcq/cgi-bin/");
 		TextField term = (TextField) s.lookup("#textfieldTerm");
@@ -50,15 +53,47 @@ public class Task3Test extends ApplicationTest {
 		
 		TableView<Section> table = (TableView<Section>) s.lookup("#table");
 		ObservableList<Section> filter_section = table.getItems();
-		
+				
 		assert(filter_section.size() != 0);
+	}
+	
+	@Test
+	public void Task3Enrolltest() {
+		TextField url = (TextField) s.lookup("#textfieldURL");
+		url.setText("http://w5.ab.ust.hk/wcq/cgi-bin/");
+		TextField term = (TextField) s.lookup("#textfieldTerm");
+		term.setText("1910");
+		TextField subject = (TextField) s.lookup("#textfieldSubject");
+		subject.setText("COMP");
+		clickOn("#buttonSearch");
 		
-		Section s = filter_section.get(0);
-		s.setEnrolled(true);
+		clickOn("#tabFilter");	
+		clickOn("#checkboxMon");
+		
+		clickOn("#tabList");
+		
+		TableView<Section> table = (TableView<Section>) s.lookup("#table");
+		
+		Node enrol = from(table).lookup(".table-row-cell").nth(0).lookup(".table-cell").nth(4).lookup(".check-box:determinate").query();
+		clickOn(enrol);
+		
+		clickOn("#tabFilter");
+		clickOn("#checkboxWed");
+		
+		CheckBox f = from(table).lookup(".table-row-cell").nth(0).lookup(".table-cell").nth(4).lookup(".check-box:determinate").query();
+		assert(f.isSelected() == true);
+		
+		clickOn("#tabMain");
+		clickOn("#buttonSearch");
 		
 		clickOn("#tabFilter");
 		clickOn("#checkboxMon");
+		clickOn("#checkboxMon");
+		
 		clickOn("#tabList");
+		
+		CheckBox c = from(table).lookup(".table-row-cell").nth(0).lookup(".table-cell").nth(4).lookup(".check-box:determinate").query();
+		assert(c.isSelected() == true);
 	}
 
 
